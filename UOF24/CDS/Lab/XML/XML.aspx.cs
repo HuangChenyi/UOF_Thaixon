d6fe03e7a0f9462e91bfa0e36fb3a725e5b6d21c
woodman <woodman@edetw.com>
2024-08-23 13:23:43 +0800

    XML LAB2

--- UOF24/CDS/Lab/XML/XML.aspx.cs
+++ UOF24/CDS/Lab/XML/XML.aspx.cs
@@ -13,30 +13,68 @@ public partial class CDS_Lab_XML_XML : BasePage
     {
         XmlDocument xmldoc=new XmlDocument();
 
-        //<ContactList/>
-        XmlElement contactListElement = xmldoc.CreateElement("ContactList");
+        XmlElement formElement = xmldoc.CreateElement("Form");
+        formElement.SetAttribute("formVersionId","0418");
+        formElement.SetAttribute("urgentLevel","2");
 
-        //<Contact name='Emma' Phone='0912345678'/>
-        XmlElement contact01Element = xmldoc.CreateElement("Contact");
-        contact01Element.SetAttribute("name","Emma");
-        contact01Element.SetAttribute("Phone", "0912345678");
+        XmlElement applicantElement = xmldoc.CreateElement("Applicant");
+        applicantElement.SetAttribute("account","Tony");
+        applicantElement.SetAttribute("groupId","");
+        applicantElement.SetAttribute("jobTitleId","");
 
-        //<Contact name='Tommy' Phone='095656565'/>
-        XmlElement contact02Element = xmldoc.CreateElement("Contact");
-        contact02Element.SetAttribute("name", "Tommy");
-        contact02Element.SetAttribute("Phone", "095656565");
+        XmlElement commentElement = xmldoc.CreateElement("Comment");
+        commentElement.InnerText = "私事處理";
 
-        //<Contact name='David' Phone='0910101010'/>
-        XmlElement contact03Element = xmldoc.CreateElement("Contact");
-        contact03Element.SetAttribute("name", "David");
-        contact03Element.SetAttribute("Phone", "0910101010");
+        XmlElement formFieldValueElement = xmldoc.CreateElement("FormFieldValue");
 
-        contactListElement.AppendChild(contact01Element);
-        contactListElement.AppendChild(contact02Element);
-        contactListElement.AppendChild(contact03Element);
+        XmlElement noElement = xmldoc.CreateElement("FieldItem");
+        noElement.SetAttribute("fieldId","NO");
+        noElement.SetAttribute("fieldValue","A001");
 
-        xmldoc.AppendChild(contactListElement);
-        txtXML.Text=xmldoc.OuterXml;
+        XmlElement a01Element = xmldoc.CreateElement("FieldItem");
+        a01Element.SetAttribute("fieldId", "A01");
+        a01Element.SetAttribute("fieldValue", "事假");
+
+        XmlElement a02Element = xmldoc.CreateElement("FieldItem");
+        a02Element.SetAttribute("fieldId", "A02");
+        a02Element.SetAttribute("fieldValue", "2018/04/18");
+
+        formElement.AppendChild(applicantElement);
+        formElement.AppendChild(formFieldValueElement);
+
+        applicantElement.AppendChild(commentElement);
+
+        formFieldValueElement.AppendChild(noElement);
+        formFieldValueElement.AppendChild(a01Element);
+        formFieldValueElement.AppendChild(a02Element);
+
+        xmldoc.AppendChild(formElement);
+        txtXML.Text = xmldoc.OuterXml;
+
+        ////<ContactList/>
+        //XmlElement contactListElement = xmldoc.CreateElement("ContactList");
+
+        ////<Contact name='Emma' Phone='0912345678'/>
+        //XmlElement contact01Element = xmldoc.CreateElement("Contact");
+        //contact01Element.SetAttribute("name","Emma");
+        //contact01Element.SetAttribute("Phone", "0912345678");
+
+        ////<Contact name='Tommy' Phone='095656565'/>
+        //XmlElement contact02Element = xmldoc.CreateElement("Contact");
+        //contact02Element.SetAttribute("name", "Tommy");
+        //contact02Element.SetAttribute("Phone", "095656565");
+
+        ////<Contact name='David' Phone='0910101010'/>
+        //XmlElement contact03Element = xmldoc.CreateElement("Contact");
+        //contact03Element.SetAttribute("name", "David");
+        //contact03Element.SetAttribute("Phone", "0910101010");
+
+        //contactListElement.AppendChild(contact01Element);
+        //contactListElement.AppendChild(contact02Element);
+        //contactListElement.AppendChild(contact03Element);
+
+        //xmldoc.AppendChild(contactListElement);
+        //txtXML.Text=xmldoc.OuterXml;
 
     }
 
@@ -45,7 +83,11 @@ public partial class CDS_Lab_XML_XML : BasePage
         XmlDocument xmlDoc =new XmlDocument();
         xmlDoc.LoadXml(txtXML.Text);
 
-        var node = xmlDoc.SelectSingleNode($"./ContactList/Contact[@name='{txtID.Text}']");
-        txtValue.Text = node.Attributes["Phone"].Value; 
+        var node = xmlDoc.SelectSingleNode($"./Form/FormFieldValue/FieldItem[@fieldId='{txtID.Text}']");
+
+        txtValue.Text = node.Attributes["fieldValue"].Value;
+
+        //var node = xmlDoc.SelectSingleNode($"./ContactList/Contact[@name='{txtID.Text}']");
+        //txtValue.Text = node.Attributes["Phone"].Value; 
     }
 }
