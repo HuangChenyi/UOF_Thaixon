24d4f5878d659421f5870b66897c143537010ba4
woodman <woodman@edetw.com>
2024-08-23 15:22:48 +0800

    申請者資訊外掛欄位

---
+++ UOF24/CDS/WKF_Fields/ApplicantInfo.ascx
@@ -0,0 +1,51 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ApplicantInfo.ascx.cs" Inherits="WKF_OptionalFields_ApplicantInfo" %>
+<%@ Reference Control="~/WKF/FormManagement/VersionFieldUserControl/VersionFieldUC.ascx" %>
+
+<table class="PopTable" style="width:500px">
+    <tr>
+        <td>
+        <asp:Label ID="Label1" runat="server" Text="公司別"></asp:Label>
+            </td>
+       <td>
+        <asp:Label ID="lblCompany" runat="server" Text=""></asp:Label>
+            </td>
+    </tr>
+    <tr>
+        <td>
+        <asp:Label ID="Label2" runat="server" Text="廠別"></asp:Label>
+            </td>
+       <td>
+        <asp:Label ID="lblFactory" runat="server" Text=""></asp:Label>
+            </td>
+    </tr>
+        <tr>
+        <td>
+        <asp:Label ID="Label3" runat="server" Text="部門名稱"></asp:Label>
+            </td>
+       <td>
+        <asp:Label ID="lblDeptName" runat="server" Text=""></asp:Label>
+            </td>
+    </tr>
+        <tr>
+    <td>
+    <asp:Label ID="Label4" runat="server" Text="部門編號"></asp:Label>
+        </td>
+   <td>
+    <asp:Label ID="lblDeptCode" runat="server" Text=""></asp:Label>
+        </td>
+</tr>
+            <tr>
+    <td>
+    <asp:Label ID="Label5" runat="server" Text="員工編號"></asp:Label>
+        </td>
+   <td>
+    <asp:Label ID="lblEmployeeNo" runat="server" Text=""></asp:Label>
+        </td>
+</tr>
+</table>
+
+<asp:Label ID="lblHasNoAuthority" runat="server" Text="無填寫權限" ForeColor="Red" Visible="False" meta:resourcekey="lblHasNoAuthorityResource1"></asp:Label>
+<asp:Label ID="lblToolTipMsg" runat="server" Text="不允許修改(唯讀)" Visible="False" meta:resourcekey="lblToolTipMsgResource1"></asp:Label>
+<asp:Label ID="lblModifier" runat="server" Visible="False" meta:resourcekey="lblModifierResource1"></asp:Label>
+<asp:Label ID="lblMsgSigner" runat="server" Text="填寫者" Visible="False" meta:resourcekey="lblMsgSignerResource1"></asp:Label>
+<asp:Label ID="lblAuthorityMsg" runat="server" Text="具填寫權限人員" Visible="False" meta:resourcekey="lblAuthorityMsgResource1"></asp:Label>
---
+++ UOF24/CDS/WKF_Fields/ApplicantInfo.ascx.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Ede.Uof.WKF.Design;
+using System.Collections.Generic;
+using Ede.Uof.WKF.Utility;
+using Ede.Uof.EIP.Organization.Util;
+using Ede.Uof.WKF.Design.Data;
+using Ede.Uof.WKF.VersionFields;
+using System.Xml;
+using System.Linq;
+
+public partial class WKF_OptionalFields_ApplicantInfo : WKF_FormManagement_VersionFieldUserControl_VersionFieldUC
+{
+
+	#region ==============公開方法及屬性==============
+    //表單設計時
+	//如果為False時,表示是在表單設計時
+    private bool m_ShowGetValueButton = true;
+    public bool ShowGetValueButton
+    {
+        get { return this.m_ShowGetValueButton; }
+        set { this.m_ShowGetValueButton = value; }
+    }
+
+    #endregion
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+		//這裡不用修改
+		//欄位的初始化資料都到SetField Method去做
+        SetField(m_versionField);
+    }    
+
+    /// <summary>
+    /// 外掛欄位的條件值
+    /// </summary>
+    public override string ConditionValue
+    {
+        get
+        {
+			//回傳字串
+			//此字串的內容將會被表單拿來當做條件判斷的值
+			return String.Empty;
+        }
+    }
+
+    /// <summary>
+    /// 是否被修改
+    /// </summary>
+    public override bool IsModified
+    {
+        get
+        {
+			//請自行判斷欄位內容是否有被修改
+			//有修改回傳True
+			//沒有修改回傳False
+            //若實作產品標準的控制修改權限必需實作
+            //一般是用 m_versionField.FieldValue (表單開啟前的值)
+            //      和this.FieldValue (當前的值) 作比對
+			return false;
+        }
+    }
+
+    /// <summary>
+    /// 查詢顯示的標題
+    /// </summary>
+    public override string DisplayTitle
+    {
+        get
+        {
+            //表單查詢或WebPart顯示的標題
+            //回傳字串
+            return $"公司別:{lblCompany.Text}-廠別:{lblFactory.Text}";
+        }
+    }
+
+    /// <summary>
+    /// 訊息通知的內容
+    /// </summary>
+    public override string Message
+    {
+        get
+        {
+            //表單訊息通知顯示的內容
+            //回傳字串
+            return $"公司別:{lblCompany.Text}-廠別:{lblFactory.Text}";
+        }
+    }
+
+
+    /// <summary>
+    /// 真實的值
+    /// </summary>
+    public override string RealValue
+    {
+        get
+        {
+            //回傳字串
+			//取得表單欄位簽核者的UsetSet字串
+            //內容必須符合EB UserSet的格式
+			return String.Empty;
+        }
+        set
+        {
+			//這個屬性不用修改
+            base.m_fieldValue = value;
+        }
+    }
+
+
+    /// <summary>
+    /// 欄位的內容
+    /// </summary>
+    public override string FieldValue
+    {
+        get
+        {
+            //回傳字串
+			//取得表單欄位填寫的內容
+
+            //<FieldValue company='公司別' factory='廠別' groupName='部門名稱' groupNo='部門編號' employeeNo='員編' />
+
+            XmlDocument xmlDoc = new XmlDocument(); 
+            XmlElement xmlElement = xmlDoc.CreateElement("FieldValue");
+            xmlElement.SetAttribute("company", lblCompany.Text);
+            xmlElement.SetAttribute("factory", lblFactory.Text);
+            xmlElement.SetAttribute("groupName", lblDeptName.Text);
+            xmlElement.SetAttribute("groupNo", lblDeptCode.Text);
+            xmlElement.SetAttribute("employeeNo", lblEmployeeNo.Text);
+            return xmlElement.OuterXml;
+
+			//return String.Empty;
+        }
+        set
+        {
+			//這個屬性不用修改
+            base.m_fieldValue = value;
+        }
+    }
+
+    /// <summary>
+    /// 是否為第一次填寫
+    /// </summary>
+    public override bool IsFirstTimeWrite
+    {
+        get
+        {
+            //這裡請自行判斷是否為第一次填寫
+            //若實作產品標準的控制修改權限必需實作
+            //實作此屬性填寫者可修改也才會生效
+            //一般是用 m_versionField.Filler == null(沒有記錄填寫者代表沒填過)
+            //      和this.FieldValue (當前的值是否為預設的空白) 作比對
+            return false;
+        }
+        set
+        {
+            //這個屬性不用修改
+            base.IsFirstTimeWrite = value;
+        }
+    }
+
+    /// <summary>
+    /// 設定元件狀態
+    /// </summary>
+    /// <param name="Enabled">是否啟用輸入元件</param>
+    public void EnabledControl(bool Enabled)
+    {
+
+    }
+
+    /// <summary>
+    /// 顯示時欄位初始值
+    /// </summary>
+    /// <param name="versionField">欄位集合</param>
+    public override void SetField(Ede.Uof.WKF.Design.VersionField versionField)
+    {
+        FieldOptional fieldOptional = versionField as FieldOptional;
+
+        if (fieldOptional != null)
+        {
+
+            //若有擴充屬性，可以用該屬性存取
+            // fieldOptional.ExtensionSetting
+
+            
+
+            if(!string.IsNullOrEmpty( base.ApplicantGuid))
+            {
+                UserUCO userUCO = new UserUCO();
+                //取得申請者資訊
+                EBUser ebUser = userUCO.GetEBUser(base.ApplicantGuid);
+                lblCompany.Text = GetFactroy(base.ApplicantGroupId);
+                lblFactory.Text = ebUser.Option2;
+                lblDeptName.Text = ebUser.GroupName;
+                lblDeptCode.Text = ebUser.Option1;
+                lblEmployeeNo.Text= ebUser.EmployeeNo;
+
+
+
+            }
+
+
+            switch(fieldOptional.FieldMode)
+            {
+                case FieldMode.Print:
+                case FieldMode.View:
+                    //觀看和列印都需作沒有權限的處理
+                    EnabledControl(false);
+                    break;
+
+            }
+            
+            #region ==============屬性說明==============『』
+			//fieldOptional.IsRequiredField『是否為必填欄位,如果是必填(True),如果不是必填(False)』
+			//fieldOptional.DisplayOnly『是否為純顯示,如果是(True),如果不是(False),一般在觀看表單及列印表單時,屬性為True』
+			//fieldOptional.HasAuthority『是否有填寫權限,如果有填寫權限(True),如果沒有填寫權限(False)』
+			//fieldOptional.FieldValue『如果已有人填寫過欄位,則此屬性為記錄其內容』
+			//fieldOptional.FieldDefault『如果欄位有預設值,則此屬性為記錄其內容』
+			//fieldOptional.FieldModify『是否允許修改,如果允許(fieldOptional.FieldModify=FieldModifyType.yes),如果不允許(fieldOptional.FieldModify=FieldModifyType.no)』
+			//fieldOptional.Modifier『如果欄位有被修改過,則Modifier的內容為EBUser,如果沒有被修改過,則會等於Null』
+            #endregion
+
+            #region ==============如果有修改，要顯示修改者資訊==============
+            if (fieldOptional.Modifier != null)
+            {
+                lblModifier.Visible = true;
+                lblModifier.ForeColor = System.Drawing.Color.FromArgb(0x52, 0x52, 0x52);
+                lblModifier.Text = System.Web.Security.AntiXss.AntiXssEncoder.HtmlEncode(fieldOptional.Modifier.Name, true);
+            } 
+            #endregion
+        }
+    }
+
+    private string GetFactroy(string groupId)
+    {
+        BaseGroup bg = new BaseGroup(groupId);
+
+        if (bg.GroupLevel == 1)
+        {
+            return bg.GroupName;
+        }
+
+        if (bg.GroupLevel == 0)
+        {
+            return "";
+        }
+        else
+        {
+            return GetFactroy(bg.ParnetGroup.GroupId);
+        }
+
+    }
+}
